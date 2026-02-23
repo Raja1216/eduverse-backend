37bd5e206e860f48405518ba546e43c48e087c19
Subhajit <subha.pramanik1212@gmail.com>
2026-02-24 00:56:34 +0530

    Code cmmmited for changes

--- database/migrations/2026_02_23_184907_add_site_id_to_cms_simple_sections_table.php
+++ database/migrations/2026_02_23_184907_add_site_id_to_cms_simple_sections_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,7 @@ return new class extends Migration
     {
         Schema::table('cms_simple_sections', function (Blueprint $table) {
             $table->foreignId('site_id')
+                ->default(1)
                 ->after('id')
                 ->constrained('sites')
                 ->cascadeOnDelete();
